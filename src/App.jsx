935376b6185c752097b000a59ccda326af884bae
Shumaila <sayedshumaila27@gmail.com>
2025-03-15 01:58:42 +0530

    Fix API url

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import MovieCard from './components/MovieCard';
 import { useDebounce } from 'react-use';
 import { getTrendingMovies, updateSearchCount } from './appwrite';
 
-const API_BASE_URL = 'https://api.themoviedb.org/3/';
+const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 const API_OPTIONS = {
